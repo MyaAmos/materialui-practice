3c3fdf92583509e425cc372d4870ef962bb8a43c
Mya Amos <peacockmya@gmail.com>
2022-06-01 15:15:38 -0400

    Added imports and made appbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
-
 import './App.css';
+import { Typography, AppBar, Card, CardActions, CardMedia, CssBaseline, Grid, Toolbar, Container } from '@material-ui/core'
+import { PhotoCamera } from '@material-ui/icons';
 
 function App() {
   return (
     <div className="App">
-      
+      <CssBaseline />
+      <AppBar position='relative'>
+        <Toolbar>
+          <PhotoCamera />
+          <Typography variant='h6'>
+            Photo Album
+          </Typography>
+        </Toolbar>
+      </AppBar>
     </div>
   );
 }
